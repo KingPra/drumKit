483b3ef4d77b2604ad7cc2c25272bc7204fd323d
KingPra <kingi.pra@gmail.com>
2017-03-07 21:56:14 -0500

    added sounds

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-
 window.addEventListener('keydown',(event) => {
-    console.log('this is a test');
+    console.log(event.keyCode);
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}']`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition () {
+    console.log(event.propertyName);
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
