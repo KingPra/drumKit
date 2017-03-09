7f1e44438db85ccf2f2920391bada14b5ae1f46d
KingPra <kingi.pra@gmail.com>
2017-03-08 19:47:30 -0500

    remove class currently not working

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-window.addEventListener('keydown',(event) => {
-    console.log(event.keyCode);
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${event.keyCode}']`);
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-});
 
-function removeTransition () {
-    console.log(event.propertyName);
+function removeTransition (event) {
+    console.log('removetransition kickd in yo');
     if (event.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+    event.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+function playSound (event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+    removeTransition(keys);
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
